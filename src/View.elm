3d0b6cfa9f4fb992b25f4931418567c49dfa68a7
Thiago Temple <ttemple@kinaxis.com>
2017-11-03 17:02:36 -0400

    adds more styling and elm graphics

--- src/View.elm
+++ src/View.elm
@@ -1,17 +1,41 @@
 module View exposing (..)
 
-import Models exposing (Model)
+import Models exposing (Model, Score)
 import Msgs exposing (Msg)
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, id, width, height)
 import Html.Events exposing (onClick)
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text "tic tac toe"
+    div [ class "flex flex-column items-center" ]
+        [ score model.score
+        , gameCanvas model
         , newGameButton
+        ]
+
+
+gameCanvas : Model -> Html Msg
+gameCanvas model =
+    div []
+        [ canvas
+            [ id "myCanvas"
+            , width 200
+            , height 200
+            ]
+            []
+        ]
+
+
+score : Score -> Html Msg
+score score =
+    div [ class "ttt-score regular h3" ]
+        [ i [ class "fa fa-user" ] []
+        , text (toString score.human)
+        , text "x"
+        , text (toString score.ai)
+        , i [ class "fa fa-mobile" ] []
         ]
 
 
@@ -19,7 +43,7 @@ newGameButton : Html Msg
 newGameButton =
     div []
         [ button
-            [ class "btn btn-primary"
+            [ class "btn btn-primary ttt-new-game"
             , onClick Msgs.NewGame
             ]
             [ i [ class "fa fa-play" ] []
